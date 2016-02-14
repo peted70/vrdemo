723b00352488b65da2e2f6cf5dac1133efb4d687
Pete Daukintis <peted70@hotmail.com>
2016-02-14 15:12:03 +0000

    added head tracking gyro code

---
+++ vrdemo/Assets/Scenes/HeadTracking.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class HeadTracking : MonoBehaviour
+{
+    private Gyroscope gyro;
+    private Vector3 initial = new Vector3(-90, 180, -90);
+
+    // Use this for initialization
+    void Start ()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+        }
+        else
+        {
+            Debug.Log("No Gyro Support");
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            var camRot = gyro.attitude;
+            transform.eulerAngles = initial;
+            transform.localRotation *= camRot;
+        }
+    }
+}
